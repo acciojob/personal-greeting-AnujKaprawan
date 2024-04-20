50623c18962ff268e2db37b5fc26e4ca70bf0cd9
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2024-04-20 17:41:30 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,33 +3,19 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [text,setText]=useState("");
+ 
+  const coppytext=(event)=>{
+    setText(event.target.value);
 
- const [name, setName] = useState('');
-  const [greeting, setGreeting] = useState('');
-
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-    setGreeting(`Hello, ${event.target.value}!`);
-  };
-
-  const handleBlur = () => {
-    setGreeting("");
-    setName("");
-  };
+  }
   return (
-<div>
-      <label>
-        Enter your name:
-        <input
-          type="text"
-          value={name}
-          onChange={handleNameChange}
-          onBlur={handleBlur}
-        />
-      </label>
-      <div>{greeting}</div>
+    <div>
+      <label htmlFor="name">Enter your name:</label>
+        <input id="name" onKeyUp={coppytext} />
+        <p>Hello {text}!</p>
     </div>
-  );
-};
+  )
+}
 
 export default App
