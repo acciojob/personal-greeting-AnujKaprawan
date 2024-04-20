ec8511c4bb9574a595771e77389c166789b019eb
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2024-04-20 17:36:28 +0530

    Updated App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+ const [name, setName] = useState('');
+  const [greeting, setGreeting] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+    setGreeting(`Hello, ${event.target.value}!`);
+  };
+
+  const handleBlur = () => {
+    setGreeting("");
+    setName("");
+  };
   return (
-    <div>
-        {/* Do not remove the main div */}
+<div>
+      <label>
+        Enter your name:
+        <input
+          type="text"
+          value={name}
+          onChange={handleNameChange}
+          onBlur={handleBlur}
+        />
+      </label>
+      <div>{greeting}</div>
     </div>
-  )
-}
+  );
+};
 
 export default App
